298e2db3b6c958740e681a4ce59ab042fc5ddcf5
Sengorius <mail@skript-manufaktur.de>
2022-08-24 12:06:03 +0200

    prevented overriding the $extension property in bundle, which results in a deprecation notice

--- src/SkriptManufakturSimpleRestBundle.php
+++ src/SkriptManufakturSimpleRestBundle.php
@@ -6,20 +6,8 @@ use SkriptManufaktur\SimpleRestBundle\DependencyInjection\SkriptManufakturSimple
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class SkriptManufakturSimpleRestBundle
- */
 class SkriptManufakturSimpleRestBundle extends Bundle
 {
-    /**
-     * @var SkriptManufakturSimpleRestExtension|null
-     */
-    protected $extension = null;
-
-
-    /**
-     * @return ExtensionInterface
-     */
     public function getContainerExtension(): ExtensionInterface
     {
         if (null === $this->extension) {
